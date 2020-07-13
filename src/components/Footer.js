96e7be3594634c3b51d28b45db9edb32a02ccec1
Frank Andrade <fr4nky.develop3r@gmail.com>
2020-07-12 23:18:39 -0400

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types'
 const Footer = props => (
   <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
     <p className="copyright">
-      &copy; Todos los derechos reservados. Built with{' '}
+      &copy; Todos los derechos reservados. Built with
       <span role="img" aria-label="heart">
-        ️️❤️
+        ️ ️❤️
       </span>{' '}
       by <a href="https://frankandrade.dev/">fr4nky</a>
     </p>
